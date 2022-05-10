df657ef8c44be4cc040f5de9c04c3fccfaf6fa2a
Masum Mollo <freelancermasummolla@gmail.com>
2022-05-10 23:02:14 +0600

    Header, Footer and Body done

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,29 +1,57 @@
 import React from 'react';
 import SliderMain from '../components/SliderMain';
-import FeatureBox from '../components/FeatureBox';
 import CarouselCollection from '../components/CarouselCollection';
 import CarouselNew from '../components/CarouselNew';
 import AuthorList from '../components/authorList';
 import Catgor from '../components/Catgor';
 import Footer from '../components/footer';
+import '../../assets/custom.scss';
 
 
 const home= () => (
   <div>
-      <section className="jumbotron breadcumb no-bg h-vh" style={{backgroundImage: `url(${'./img/bg-shape-1.jpg'})`}}>
+    
+    //Hero Section / Banner Section here
+      <section className="jumbotron breadcumb no-bg h-vh" style={{backgroundImage: `url(${'./img/bg-shape-1-dark.jpg'})`}}>
+        
+        //Inner Banner Section
          <SliderMain/>
       </section>
-
-      <section className='container no-top no-bottom'>
-        <FeatureBox/>
-      </section>
-
       <section className='container no-bottom'>
         <div className='row'>
           <div className='col-lg-12'>
             <div className='text-center'>
-              <h2>Hot Collections</h2>
+              <h2 className='mb-5 text-white'>Browse by category</h2>
               <div className="small-border"></div>
+            </div>
+          </div>
+        </div>
+
+        // Category Section here
+        <Catgor/>
+      </section>
+
+      // New Items Section here
+      <section className='container no-bottom'>
+        <div className='row'>
+          <div className='col-lg-12'> 
+            <div className='text-center'>
+              <h2 className='text-white mb-5'>New Items</h2>
+              <div className="small-border"></div>
+            </div>
+          </div>
+          <div className='col-lg-12'>
+            <CarouselNew/>
+          </div>
+         </div>
+       </section>
+
+       // Hot Collection Section here
+      <section className='container no-bottom'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='text-center'>
+              <h2 className='text-white mb-5'>Hot Collections</h2>
             </div>
           </div>
           <div className='col-lg-12'>
@@ -31,26 +59,13 @@ const home= () => (
           </div>
         </div>
       </section>
-
+      
+      //Top Seller Section here
       <section className='container no-bottom'>
         <div className='row'>
           <div className='col-lg-12'>
             <div className='text-center'>
-              <h2>New Items</h2>
-              <div className="small-border"></div>
-            </div>
-          </div>
-          <div className='col-lg-12'>
-            <CarouselNew/>
-          </div>
-        </div>
-      </section>
-
-      <section className='container no-bottom'>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <div className='text-center'>
-              <h2>Top Sellers</h2>
+              <h2 className='text-white mb-5'>Top Sellers</h2>
               <div className="small-border"></div>
             </div>
           </div>
@@ -60,20 +75,8 @@ const home= () => (
         </div>
       </section>
 
-      <section className='container'>
-        <div className='row'>
-          <div className='col-lg-12'>
-            <div className='text-center'>
-              <h2>Browse by category</h2>
-              <div className="small-border"></div>
-            </div>
-          </div>
-        </div>
-        <Catgor/>
-      </section>
-
+      // Footer Section here
     <Footer />
-
   </div>
 );
 export default home;
